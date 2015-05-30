f82e2f447e0733c298b90907e1970b4f6e5876b6
Mayank Mandava <mayankmandava@gmail.com>
2015-05-30 02:41:37 -0500

    cg working

--- hw4/MG/cg.c
+++ hw4/MG/cg.c
@@ -6,31 +6,29 @@
 double dotprod(int n, double **r1, double **r2) {
     double rdot = 0;
     for (int i = 1; i <= n; ++i)
-       for(int j = 1; j <= n; ++j) 
+        for(int j = 1; j <= n; ++j) 
             rdot += r1[i][j] * r2[i][j];
     return rdot;
 }
 
-void linear_comb(int n, double **r1, double a, double **r2, double b, double **r3){
-    //r1 = a*r2 + b*r3
-    for (int i = 1; i <= n; ++i) 
-       for(int j = 1; j <= n; ++j) 
-          r1[i][j] = a*r2[i][j] + b*r3[i][j];
-}
-
-void cg(double **b, double **x, int n) {
+void cg(double **xold, double **x, int n) {
     double alpha;
     double beta;
     double **r = dmatrix(1,n,1,n);
     double **p = dmatrix(1,n,1,n);
     double **Ap = dmatrix(1,n,1,n);
-    double **Ax = dmatrix(1,n,1,n);
-    double **aux = dmatrix(1,n,1,n);
 
     //r = b - Ax
-    copy(Ax, x, n);
-    bi_diagonal_prod(-C, n, aux, Ax); //multiplies argument by A
-    linear_comb(n, r, 1, b, -1, Ax);
+    for(int i = 2; i < n; ++i) 
+        for(int j = 2; j < n; ++j) {
+            double b = (1-4*C)*xold[i][j] + 
+                C*(xold[i-1][j] + xold[i+1][j] + xold[i][j-1] + xold[i][j+1]);
+            r[i][j] = b - (1+4*C)*x[i][j] + 
+                C*(x[i-1][j] + x[i+1][j] + x[i][j-1] + x[i][j+1]);
+        }
+    //boundaries of r
+    for(int i = 1; i <= n; ++i)
+        r[i][1] = r[i][n] = r[1][i] = r[n][i] = 0;
 
     //p = r
     copy(p, r, n);
@@ -42,29 +40,37 @@ void cg(double **b, double **x, int n) {
     int k;
     for (k = 0; k < n; ++k) {
         //Ap = A*p
-        copy(Ap, p, n);
-        bi_diagonal_prod(-C, n, aux, Ap);
+        for(int i = 2; i < n; ++i) 
+            for(int j = 2; j < n; ++j) 
+                Ap[i][j] = (1+4*C)*p[i][j] 
+                    - C*(p[i-1][j] + p[i+1][j] + p[i][j-1] + p[i][j+1]);
+        for(int i = 1; i <= n; ++i)
+            Ap[i][1] = Ap[i][n] = Ap[1][i] = Ap[n][i] = 0;
 
         double pAp = dotprod(n, p, Ap);
         alpha = rsold / pAp;
 
-        linear_comb(n, x, 1.0, x, alpha, p);
-        linear_comb(n, r, 1.0, r, -alpha, Ap);
+        for(int i = 2; i < n; ++i) 
+            for(int j = 2; j < n; ++j) {
+                x[i][j] += alpha*p[i][j];
+                r[i][j] -= alpha*Ap[i][j];
+            }
 
         rsnew = dotprod(n, r, r);
         if (rsnew < 1e-10) break;
 
         beta = rsnew / rsold;
-        linear_comb(n, p, 1.0, r, beta, p);
+        for(int i = 2; i < n; ++i) 
+            for(int j = 2; j < n; ++j) 
+                p[i][j] = r[i][j] + beta*p[i][j];
         rsold = rsnew;
     }
 
     printf("%d\n", k);
-    copy(b, x, n); //copy solution into b (implicit return)
+    copy(xold, x, n); //copy solution into xold (implicit return)
 
     free_dmatrix(r, 1, n, 1, n);
     free_dmatrix(p, 1, n, 1, n);
     free_dmatrix(Ap, 1, n, 1, n);
-    free_dmatrix(Ax, 1, n, 1, n);
-    free_dmatrix(aux, 1, n, 1, n);
 }
+
